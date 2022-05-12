4d6b4a19863f26547a6434f5d894cfae91078018
Kaak <cours.kaak@gmail.com>
2022-05-12 22:11:25 +0200

    avant ajout page accueil

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<style></style>
+
+<style scoped></style>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
